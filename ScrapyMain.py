b6e2915a8c5510a7982f1ab38428ea5f737c48de
JayWHodgson <jaywhodgson@gmail.com>
2021-03-01 16:14:38 -0400

    Uploads attachments to own server
    On deletion of channel attachments will no long be lost

--- ScrapyMain.py
+++ ScrapyMain.py
@@ -6,11 +6,14 @@ import discord
 import os
 import re
 import shlex
+import requests
+import shutil
 
 #setup discord
 TOKEN = ''
 with open('Token.txt', 'r') as f:
     TOKEN = f.readline()
+ISTXCID = 816008780911214618
 client = discord.Client()
 
 #make a text log of scrape_target and post it in post_target
@@ -23,8 +26,8 @@ async def log_make(scrape_target, post_target):
         iNum+=1
     
     last_auth = -1 #declare id to compare to
-    async for i in scrape_target.history(oldest_first=True):
-        with open(filename,'a', encoding='utf8') as f:
+    with open(filename,'w+', encoding='utf8') as f:
+        async for i in scrape_target.history(oldest_first=True):
             #don't include messages by the bot and the command to activate it
             if (i.author != client.user) and (re.search('!Scrape*', i.content) == None):
                 #if the message has a different author than the last message write their name to file
@@ -36,25 +39,31 @@ async def log_make(scrape_target, post_target):
                 
                 #write attachment urls to file if they exists
                 for q in i.attachments:
-                    f.write(q.url)
-                    #seperate urls
-                    if q != i.attachments[len(i.attachments) - 1]:  
-                        f.write(' / ')
+                    atturl = await image_get(q.url)
+                    if atturl: #if image was successfully retrieved
+                        f.write(atturl)
+                        #seperate urls
+                        if q != i.attachments[len(i.attachments) - 1]:  
+                            f.write(' / ')
                 f.write('\n')
     #post to channel of choice
     await post_target.send(file=discord.File(filename))
     os.remove(filename) #remove file once uploaded
     return
 
-#find a channel in a server with a name string
-async def channel_find(name, guild):
-    found = False
-    for j in guild.channels:
-        if name == j.name:
-            found = True
-            mChnl = j
-            return j
-    return None
+async def image_get(url):
+    image_server = client.get_channel(816008780911214618)
+    filename = url.split("/")[-1] #make a file name
+    r = requests.get(url, stream=True) #attempt to retrieve image
+    if r.status_code == 200: #check if image was retrieved
+        r.raw.decode_content = True #if not set image size will be zero
+        with open(filename, 'wb') as f: #write raw data to a file in binary
+            shutil.copyfileobj(r.raw, f)
+        message = await image_server.send(file=discord.File(filename)) #send attachment to image server
+        os.remove(filename) #remove file once uploaded
+        return message.attachments[0].url #there will only ever be one attachment
+    else:
+        return None
 
 @client.event
 async def on_ready():
@@ -79,9 +88,9 @@ async def on_message(message):
                 return
             
             #get source and destination channels
-            scrape_target = await channel_find(args[1], message.guild)
-            post_target =  await channel_find(args[2], message.guild)
-            
+            scrape_target = discord.utils.get(message.guild.channels, name=args[1])
+            post_target = discord.utils.get(message.guild.channels, name=args[2])
+
             #check to see if the channels exist
             if (scrape_target == None) or (post_target == None):
                 await message.channel.send(content='Channel not found')
